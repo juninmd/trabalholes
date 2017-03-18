997e91e2403f164e6749f854aa6ee2517cd8c473
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-03-17 23:58:41 -0300

    INIT

---
+++ query.sql
@@ -0,0 +1,12 @@
+Select * from pais ;
+select * from pais p order by p.painome;
+select p.painome from pais p order by p.painome;
+select p.painome from pais p where p.paicodi = 1058 order by p.painome        ;
+
+
+select *  from estado;
+select * from estado e order by e.estnome;
+select e.estnome from estado e order by e.estnome;
+select e.estnome from estado e where e.estcodi = 35 order by e.esticms;
+
+
