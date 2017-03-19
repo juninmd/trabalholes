c9e90f037f9bded4fddce80b41fd5ef31feb6b71
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-03-19 16:43:26 -0300

    Crud Países

---
+++ LES_17_I_N/PaisModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LES_17_I_N
+{
+    public class PaisModel
+    {
+        public int PAICODI { get; set; }
+        public string PAINOME { get; set; }
+    }
+}
