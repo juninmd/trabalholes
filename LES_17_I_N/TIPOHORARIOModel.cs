3a325ed4b3d9c5dbdf62540068d88d5d856433d3
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2017-03-20 21:09:25 -0300

    adicionado tipo de horário.

--- LES_17_I_N/TIPOHORARIOModel.cs
+++ LES_17_I_N/TIPOHORARIOModel.cs
@@ -5,7 +5,7 @@ namespace LES_17_I_N
     public class TipohorarioModel
     {
 
-         public string THCODI { get; set; }
+         public int THCODI { get; set; }
 
          public string THNOME { get; set; }
 
---
+++ LES_17_I_N/frmtipohorario.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace LES_17_I_N
+{
+    partial class frmtipohorario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbctip = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dgvtip = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnvoltar = new System.Windows.Forms.Button();
+            this.btnexcluir = new System.Windows.Forms.Button();
+            this.btnincluir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txttipnome = new System.Windows.Forms.TextBox();
+            this.txttipcodi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbctip.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvtip)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbctip
+            // 
+            this.tbctip.Controls.Add(this.tabPage1);
+            this.tbctip.Controls.Add(this.tabPage2);
+            this.tbctip.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbctip.Location = new System.Drawing.Point(0, 0);
+            this.tbctip.Name = "tbctip";
+            this.tbctip.SelectedIndex = 0;
+            this.tbctip.Size = new System.Drawing.Size(384, 361);
+            this.tbctip.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dgvtip);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(376, 335);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Listagem";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dgvtip
+            // 
+            this.dgvtip.AllowUserToAddRows = false;
+            this.dgvtip.AllowUserToDeleteRows = false;
+            this.dgvtip.AllowUserToOrderColumns = true;
+            this.dgvtip.AllowUserToResizeRows = false;
+            this.dgvtip.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvtip.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvtip.Location = new System.Drawing.Point(3, 3);
+            this.dgvtip.Name = "dgvtip";
+            this.dgvtip.ReadOnly = true;
+            this.dgvtip.RowHeadersVisible = false;
+            this.dgvtip.Size = new System.Drawing.Size(370, 329);
+            this.dgvtip.TabIndex = 0;
+            this.dgvtip.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvtip_CellDoubleClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnvoltar);
+            this.tabPage2.Controls.Add(this.btnexcluir);
+            this.tabPage2.Controls.Add(this.btnincluir);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.txttipnome);
+            this.tabPage2.Controls.Add(this.txttipcodi);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(376, 335);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Detalhado";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnvoltar
+            // 
+            this.btnvoltar.Location = new System.Drawing.Point(275, 294);
+            this.btnvoltar.Name = "btnvoltar";
+            this.btnvoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnvoltar.TabIndex = 7;
+            this.btnvoltar.Text = "Voltar";
+            this.btnvoltar.UseVisualStyleBackColor = true;
+            this.btnvoltar.Click += new System.EventHandler(this.btnvoltar_Click);
+            // 
+            // btnexcluir
+            // 
+            this.btnexcluir.Location = new System.Drawing.Point(194, 294);
+            this.btnexcluir.Name = "btnexcluir";
+            this.btnexcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnexcluir.TabIndex = 6;
+            this.btnexcluir.Text = "Excluir";
+            this.btnexcluir.UseVisualStyleBackColor = true;
+            this.btnexcluir.Click += new System.EventHandler(this.btnexcluir_Click);
+            // 
+            // btnincluir
+            // 
+            this.btnincluir.Location = new System.Drawing.Point(32, 294);
+            this.btnincluir.Name = "btnincluir";
+            this.btnincluir.Size = new System.Drawing.Size(156, 23);
+            this.btnincluir.TabIndex = 4;
+            this.btnincluir.Text = "Incluir / Gravar";
+            this.btnincluir.UseVisualStyleBackColor = true;
+            this.btnincluir.Click += new System.EventHandler(this.btnincluir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome:";
+            // 
+            // txttipnome
+            // 
+            this.txttipnome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txttipnome.Location = new System.Drawing.Point(118, 55);
+            this.txttipnome.Name = "txttipnome";
+            this.txttipnome.Size = new System.Drawing.Size(250, 20);
+            this.txttipnome.TabIndex = 2;
+            // 
+            // txttipcodi
+            // 
+            this.txttipcodi.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txttipcodi.Location = new System.Drawing.Point(118, 29);
+            this.txttipcodi.Name = "txttipcodi";
+            this.txttipcodi.Size = new System.Drawing.Size(50, 20);
+            this.txttipcodi.TabIndex = 1;
+            this.txttipcodi.Leave += new System.EventHandler(this.txttipcodi_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // frmtipohorario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.tbctip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmtipohorario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manutenção de Tipos de Horário";
+            this.Load += new System.EventHandler(this.frmtipohorario_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmtipohorario_KeyDown);
+            this.tbctip.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvtip)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tbctip;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txttipnome;
+        private System.Windows.Forms.TextBox txttipcodi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnvoltar;
+        private System.Windows.Forms.Button btnexcluir;
+        private System.Windows.Forms.Button btnincluir;
+        private System.Windows.Forms.DataGridView dgvtip;
+    }
+}
