f45e8ef16f8223ace6f37307934da7d4bcff0b37
juninmd <jr_acn@yahoo.com.br>
2017-06-24 18:51:52 -0300

    pedido

--- LES_17_I_N/PedidoModel.cs
+++ LES_17_I_N/PedidoModel.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace LES_17_I_N
 {
     public class PedidoModel
@@ -8,34 +10,34 @@ namespace LES_17_I_N
         /// <summary>
         /// Data de Emissão
         /// </summary>
-        public string PEDDTEM { get; set; }
+        public DateTime PEDDTEM { get; set; }
 
         /// <summary>
         /// Data de Baixa
         /// </summary>
-        public string PEDDTBA { get; set; }
+        public DateTime PEDDTBA { get; set; }
 
         /// <summary>
         /// Total do Pedido
         /// </summary>
-        public string PEDTOTA { get; set; }
+        public double PEDTOTA { get; set; }
 
 
         /// <summary>
         /// Percentual de Desconto
         /// </summary>
-        public string PEDPERC { get; set; }
+        public double PEDPERC { get; set; }
 
 
         /// <summary>
         /// Valor de Desconto
         /// </summary>
-        public string PEDDESC { get; set; }
+        public double PEDDESC { get; set; }
 
         /// <summary>
         /// PEDTOTA - PEDDESC
         /// </summary>
-        public string PEDLIQU { get; set; }
+        public double PEDLIQU { get; set; }
 
         /// <summary>
         /// Condição de Pagamento
@@ -50,12 +52,12 @@ namespace LES_17_I_N
         /// <summary>
         /// Cli Codi
         /// </summary>
-        public string CLICODI { get; set; }
+        public int CLICODI { get; set; }
 
         /// <summary>
         /// Vendedor Codi
         /// </summary>
-        public string FUNCODI { get; set; }
+        public int FUNCODI { get; set; }
 
     }
 }
